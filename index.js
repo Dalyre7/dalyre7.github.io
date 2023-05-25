99e04e50d9410cec1bacd7a5035d9cba92eed776
Dalyre7 <110495237+Dalyre7@users.noreply.github.com>
2023-05-25 19:19:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+document.getElementById("btn").onclick = function() {
+    let text = document.getElementById("text").value;
+    console.log(text);
+    window.alert("Thanks, your response has been recorded.")
+}
+
+
+
+
